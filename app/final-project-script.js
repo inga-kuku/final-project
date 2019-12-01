6b2eb0e980e24f28cd71b8543b5526ffc57e67c0
Inga Kutkaite <ingakut@Ingas-MacBook-Pro-2.local>
2019-12-01 21:59:33 +0200

    swiper fixes

--- app/final-project-script.js
+++ app/final-project-script.js
@@ -16,6 +16,7 @@ var mySwiper = new Swiper ('.swiper-container', {
     },
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
     },
     navigation: {
       nextEl: '.swiper-button-next',
